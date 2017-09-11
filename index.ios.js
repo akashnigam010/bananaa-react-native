e8f3022409fe166a190da93ecd8e975b201db7da
Akash Nigam <akashnigam010@gmail.com>
2017-09-12 01:51:02 +0530

    Added FB Login support for ios and android

--- index.ios.js
+++ index.ios.js
@@ -1,14 +1,41 @@
 import React, {Component} from 'react';
-import {AppRegistry, Text, View} from 'react-native';
+import {AppRegistry, Text, View, TouchableOpacity} from 'react-native';
 
 import Component1 from './app/components/Component1';
+import FBSDK, { LoginManager } from 'react-native-fbsdk';
 
 export default class bnaApp extends Component {
-  render(){
-    return(
-      <Component1 />
-    );
-  }
+
+	_fbAuth() {
+		LoginManager.logInWithReadPermissions(['public_profile', 'email']).then(
+		  function(result) {
+		    if (result.isCancelled) {
+		      alert('Login cancelled');
+		    } else {
+		      alert('Login success with permissions: '
+		        +result.grantedPermissions.toString());
+		    }
+		  },
+		  function(error) {
+		    alert('Login fail with error: ' + error);
+		  }
+		);
+	}
+
+	render(){
+		return(
+		  <View>
+		  	<Text>Hello</Text>
+		  	<Text>Hello</Text>
+		  	<Text>Hello</Text>
+		  	<Text>Hello</Text>
+		  	<Text>Hello</Text>
+		  	<TouchableOpacity onPress={this._fbAuth}>
+		  		<Text>Login with Facebook</Text>
+		  	</TouchableOpacity>
+		  </View>
+		);
+	}
 }
 
 AppRegistry.registerComponent('bnaApp', () => bnaApp);
